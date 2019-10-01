2ce5c31e28adbe08980d7d04ec91d66fe8d3ee02
kbales9 <55754403+kbales9@users.noreply.github.com>
2019-10-01 12:28:44 -0600

    Delete jsstalker.js

--- jsstalker.js
+++
@@ -1,6 +0,0 @@
-var firstName = prompt ("What is your first name?");
-var lastName = prompt("What is your last name?");
-var age = prompt ("How old are you?");
-var fullName = firstName + " " + lastName
-console.log( "Your name is " + fullName);
-console.log("You are " + age + " years old");
